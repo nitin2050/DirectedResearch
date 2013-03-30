c1eab95b3121930ebf7bbdab1d3f8a17cdbfda81
yumingon <yumingon@usc.edu>
2013-03-29 17:14:06 -0700

    moveto() of Queen and Biship are modified according to req.7

--- src/ChessAPI/Bishop.java
+++ src/ChessAPI/Bishop.java
@@ -9,12 +9,13 @@ public class Bishop extends Piece{
 	public Bishop(Color c, Square s, Type t) {
 		super(c,s,t);
 	}
-
+	public String err="";
+	
 	@Override
 	public boolean moveTo(Square destination) {
 		
 		System.out.println("Bishop");
-		if(destination.getPiece()==null){
+		if(this.validateMove(this.getSquare(), destination)){
 			System.out.println("Piece moved to "+destination.get_x()+","+destination.get_y());
 			//move piece to destination 
 			destination.setPiece(this);
@@ -22,17 +23,75 @@ public class Bishop extends Piece{
 			this.setSquare(destination);
 			return true;			
 		}else{
-			System.out.println("the move is not completed due to existance of another piece in the destination square");
+			System.out.println(err);
 			return false;
 		}
-		//logic for checking if the Bishop can move from current Location to this Destination
+		//logic for checking if the Queen can move from current Location to this Destination
 		//i.e validate()
 		//return true and move else return false
 		
-
+	}		
+	
+	private boolean validateMove(Square s, Square d){
+		boolean decision = true;
+		if(d.get_x()>8 || d.get_y()>8 || d.get_x()<1 || d.get_y()<1){
+			decision = false;
+			err="Destination square is not a part of the game board, the requested moved is rejected.";
+			return decision;
+		}
+		if(s.get_x()==d.get_x() && s.get_y()==d.get_y()){
+			decision = false;
+			err="Source square and destination square can not be the same, the requested move is rejected.";
+			return decision;
+		}
+		if(!this.validateAgainstRule(s, d)){
+			decision=false;
+			err="Not a valid move for a Biship, the requested move is rejected.";
+			return decision;
+		}
+		if(this.isObstructed(s, d)){
+			decision=false;
+			err="Another piece exists in the path to the destination square, the requested move is rejected.";
+		}
+		return decision;
+	}
+	
+	private boolean validateAgainstRule(Square s, Square d) {
+		boolean result = false; 
+		int diff_x = Math.abs(s.get_x()-d.get_x());
+		int diff_y = Math.abs(s.get_y() - d.get_y());
+		if(diff_x == diff_y)
+			result = true;			
+		return result;
 	}
 	
 	
+	private boolean isObstructed(Square s, Square d) {
+		Board board = new Board();
+		boolean result = true;		
+		int s_x = s.get_x();
+		int s_y = s.get_y();
+		int d_x = d.get_x();
+		int d_y = d.get_y();
+		int diff_x = d_x - s_x;
+		int diff_y = d_y - s_y;
+
+		while(s_x != d_x || s_y != d_y){
+			s_x=s_x+diff_x/Math.abs(diff_x);
+			s_y=s_y+diff_y/Math.abs(diff_y);
+			if(board.getSquare(s_x, s_y).getPiece() != null){
+				result = false;
+				break;
+			}
+		}
+		return result;
+	}
+	
+	
+	
+	
+	
 	//the class will also include other functions specific to Bishop Object
+	
 	
 }
--- src/ChessAPI/Queen.java
+++ src/ChessAPI/Queen.java
@@ -1,5 +1,6 @@
 package ChessAPI;
 
+
 public class Queen extends Piece{
 	
 	public Queen() {
@@ -9,12 +10,13 @@ public class Queen extends Piece{
 	public Queen(Color c, Square s, Type t) {
 		super(c,s,t);
 	}
+	public String err="";
 
 	@Override
 	public boolean moveTo(Square destination) {
 		
 		System.out.println("Queen");
-		if(destination.getPiece()==null){
+		if(this.validateMove(this.getSquare(), destination)){
 			System.out.println("Piece moved to "+destination.get_x()+","+destination.get_y());
 			//move piece to destination 
 			destination.setPiece(this);
@@ -22,7 +24,7 @@ public class Queen extends Piece{
 			this.setSquare(destination);
 			return true;			
 		}else{
-			System.out.println("the move is not completed due to existance of another piece in the destination square");
+			System.out.println(err);
 			return false;
 		}
 		//logic for checking if the Queen can move from current Location to this Destination
@@ -30,6 +32,76 @@ public class Queen extends Piece{
 		//return true and move else return false
 		
 	}
+	
+	//validates the requested move
+	private boolean validateMove(Square s, Square d){
+		boolean decision = true;
+		if(d.get_x()>8 || d.get_y()>8 || d.get_x()<1 || d.get_y()<1){
+			decision = false;
+			err="Destination square is not a part of the game board, the requested moved is rejected.";
+			return decision;
+		}
+		if(s.get_x()==d.get_x() && s.get_y()==d.get_y()){
+			decision = false;
+			err="Source square and destination square can not be the same, the requested move is rejected.";
+			return decision;
+		}
+		if(!this.validateAgainstRule(s, d)){
+			decision=false;
+			err="Not a valid move for a Queen, the requested move is rejected.";
+			return decision;
+		}
+		if(this.isObstructed(s, d)){
+			decision=false;
+			err="Another piece exists in the path to the destination square, the requested move is rejected.";
+		}
+		return decision;
+	}
+
+	private boolean isObstructed(Square s, Square d) {
+		Board board = new Board();
+		boolean result = true;		
+		int s_x = s.get_x();
+		int s_y = s.get_y();
+		int d_x = d.get_x();
+		int d_y = d.get_y();
+		int diff_x = d_x - s_x;
+		int diff_y = d_y - s_y;
+		if(s_x==d_x){
+			for(int i = s_y+diff_y/Math.abs(diff_y); i<=d_y; i=i+diff_y/Math.abs(diff_y)){
+				if(board.getSquare(s_x, i).getPiece() != null)
+					result = false;
+			}
+		}
+		
+		if(s_y==d_y){
+			for(int i = s_x+diff_x/Math.abs(diff_x); i<=d_x; i=i+diff_x/Math.abs(diff_x)){
+				if(board.getSquare(i, s_y).getPiece() != null)
+					result = false;
+			}
+		}
+
+		while(s_x != d_x || s_y != d_y){
+			s_x=s_x+diff_x/Math.abs(diff_x);
+			s_y=s_y+diff_y/Math.abs(diff_y);
+			if(board.getSquare(s_x, s_y).getPiece() != null){
+				result = false;
+				break;
+			}
+		}
+		return result;
+	}
+
+	private boolean validateAgainstRule(Square s, Square d) {
+		boolean result = false; 
+		int diff_x = Math.abs(s.get_x()-d.get_x());
+		int diff_y = Math.abs(s.get_y() - d.get_y());
+		if(s.get_x()==d.get_x() || s.get_y()==d.get_y() || diff_x == diff_y)
+			result = true;			
+		return result;
+	}
+	
+
 	
 	
 	//the class will also include other functions specific to Queen Object
